8dac839f14a8381d19bb3cc6bd0eea56422b07bb
nishhdgop <108075033+nishhdgop@users.noreply.github.com>
2022-10-19 19:31:26 +0530

    Add files via upload

---
+++ cryptoExecption.java
@@ -0,0 +1,26 @@
+package gui;
+
+public class cryptoExecption extends Exception {
+	
+
+	   public cryptoExecption ()
+	   {
+	      super ();
+	   }
+
+	   public cryptoExecption (String message)
+	   {
+	      super (message);
+	   }
+
+	   public cryptoExecption (Throwable cause)
+	   {
+	      super (cause);
+	   }
+
+	   public cryptoExecption (String message, Throwable cause)
+	   {
+	      super (message, cause);
+	   }
+	}
+
---
+++ loginMain.java
@@ -0,0 +1,14 @@
+package gui;
+
+public class loginMain {
+
+
+
+	public static void main(String[] args) {
+		
+		IDandPasswords idandPasswords = new IDandPasswords();
+				
+		LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+
+	}
+}
